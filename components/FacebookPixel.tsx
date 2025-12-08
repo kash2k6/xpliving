0e919dd78f14ebe38e0368bbab570e11a0c9098a
Kashief <Kashiefhenry@gmail.com>
2025-12-08 09:44:04 -0500

    Update Facebook Pixel ID to 1375515147300870

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -15,7 +15,7 @@ declare global {
 }
 
 export default function FacebookPixel() {
-  const pixelId = '836178942642631';
+  const pixelId = '1375515147300870';
 
   return (
     <>
@@ -32,7 +32,7 @@ export default function FacebookPixel() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '836178942642631');
+            fbq('init', '1375515147300870');
             fbq('track', 'PageView');
           `,
         }}
@@ -42,7 +42,7 @@ export default function FacebookPixel() {
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=836178942642631&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=1375515147300870&ev=PageView&noscript=1"
           alt=""
         />
       </noscript>
